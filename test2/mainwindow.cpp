17ac334a083d42f73dc282c07a4d56a18b03c5b5
dxqzhuang <34282801+dxqzhuang@users.noreply.github.com>
2018-05-28 01:57:05 -0700

    functional while loop
    total number of entries is 35154; reduce number of entries when debugging.
    35154 entries takes approximately 2 minutes to process on my laptop, results may vary.
    ui->append line before entry++ for debugging purpose only; should just write out to file instead

--- test2/mainwindow.cpp
+++ test2/mainwindow.cpp
@@ -32,97 +32,85 @@ void MainWindow::readFile(const QString &fileName)
     QFile input(fileName);
     input.open(QFile::ReadOnly);
     fileContents.clear();
-    QString line;       //to be manipulated by the function
-    QString subLine;    //to be used for push_back vectors
-//    while(!input.atEnd())
-//        if(input.getChar(&data))
-//            fileContents += data;
-    ushort index;
-    QChar qdata;
-    for(size_t i =0; i<178; ++i){   //read from .bat file
+    QString line;                       //to be manipulated by the function
+
+    while(!input.atEnd()){              //read from .bat file
         if(input.getChar(&data)){
             fileContents+=data;
         }
     }
 
+    fileContents.remove(0,107);
 
-    line=fileContents;
-    line.remove(0,109);
-    subLine="";
-    QChar temp;
-//    while(1){
+    size_t entry=0;
+    while(entry<35154){                  //total entries: 35154
+        fileContents.remove(0,2);       //clean up 06 and 04
+                                        //assuming 06 is bin size
+                                        //unsure what 04 means
+                                        //however sizevector not necessary
+        //0. reading in size info
+        CT1Size.push_back(fileContents[0].unicode());
+        CT2Size.push_back(fileContents[1].unicode());
+        countrySize.push_back(fileContents[2].unicode());
+        phoneSize.push_back(fileContents[3].unicode());
+        fileContents.remove(0,4);
 
-    //0. reading in size info
-    CT1Size.push_back(line[0].unicode());
-    CT2Size.push_back(line[1].unicode());
-    countrySize.push_back(line[2].unicode());
-    phoneSize.push_back(line[3].unicode());
-    line.remove(0,4);
+        //1. reading in BIN
+        for(int i=0; i<6; i++){
+            line+=fileContents[i];
+        }
+        bin.push_back(line);
+        fileContents.remove(0,6);   //clean up
+        line = "";
 
-    //1. reading in BIN
-    for(int i=0; i<6; i++){
-        subLine+=line[i];
+        //2. read in stringSize for bank name(unicode, 7th digit)
+        nameSize.push_back(fileContents[0].unicode()); //char to ushort
+
+        //disregard size and 3 null chars
+        fileContents.remove(0,4);   //clean up
+
+        //3. read in bank name//nameSize[j] fix counter later
+        for(ushort i=0; i<(nameSize[entry]); ++i){
+            line+=fileContents[i];
+        }
+        bankName.push_back(line);
+
+        fileContents.remove(0,int(nameSize[entry]));    //clean up
+        line = "";
+
+        //4. read in cardtype1
+        for(ushort i=0; i<CT1Size[entry];++i){
+            line+=fileContents[i];
+        }
+        cardType1.push_back(line);
+
+        fileContents.remove(0,int(CT1Size[entry]));    //clean up
+        line = "";
+
+        //5. read in cardtype2
+        for(ushort i=0; i<CT2Size[entry];++i){
+            line+=line[i];
+        }
+        cardType2.push_back(line);
+        fileContents.remove(0,int(CT2Size[entry]));    //clean up
+        line = "";
+
+        //6.read in country
+        for(ushort i=0; i<countrySize[entry];++i){
+            line+=fileContents[i];
+        }
+        country.push_back(line);
+
+        fileContents.remove(0,int(countrySize[entry]));    //clean up
+        line = "";
+        //remove junk
+        fileContents.remove(0,int(phoneSize[entry]));    //clean up phone numbers
+        ui->fileContentsDisplay->append(bin[entry]);
+        entry++;
     }
-    bin.push_back(subLine);
-    line.remove(0,6);   //clean up
-    subLine = "";       //clearing subline
-
-    //2. read in stringSize for bank name(unicode, 7th digit)
-    nameSize.push_back(line[0].unicode()); //char to ushort
-
-    //disregard size and 3 null chars
-    line.remove(0,4);   //clean up
-
-    //3. read in bank name//nameSize[j] fix counter later
-    for(ushort i=0; i<(nameSize[0]); ++i){
-        subLine+=line[i];
-    }
-    bankName.push_back(subLine);
-
-    line.remove(0,int(nameSize[0]));    //clean up
-    subLine = "";
-
-    //4. read in cardtype1
-    for(ushort i=0; i<CT1Size[0];++i){
-        subLine+=line[i];
-    }
-    cardType1.push_back(subLine);
-
-    line.remove(0,int(CT1Size[0]));    //clean up
-    subLine = "";
-
-    //5. read in cardtype2
-    for(ushort i=0; i<CT2Size[0];++i){
-        subLine+=line[i];
-    }
-    cardType2.push_back(subLine);
-    line.remove(0,int(CT2Size[0]));    //clean up
-    subLine = "";
-    //6.read in country
-    for(ushort i=0; i<countrySize[0];++i){
-        subLine+=line[i];
-    }
-    country.push_back(subLine);
-    line.remove(0,int(countrySize[0]));    //clean up
-    subLine = "";
-    //remove junk
-    line.remove(0,int(phoneSize[0]));    //clean up phone numbers
-
-//    }
-
-
-
-
-
-
     input.close();
-//    ui->fileContentsDisplay->clear();
-    ui->fileContentsDisplay->append(bin[0]);
-    ui->fileContentsDisplay->append(bankName[0]);
-    ui->fileContentsDisplay->append(cardType1[0]);
-    ui->fileContentsDisplay->append(cardType2[0]);
-    ui->fileContentsDisplay->append(country[0]);
 }
+
 
 
 void MainWindow::openSaveFile()
